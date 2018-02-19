63dbd8f9992bd304c50a384af6dd4cefba9b699e
Punisher27 <36614304+Punisher27@users.noreply.github.com>
2018-02-18 22:37:16 -0500

    Add files via upload
    First Submission

---
+++ P2T1_SalesPredictionSolution_BrownChristopher.py
@@ -0,0 +1,7 @@
+Python 3.6.4 (v3.6.4:d48eceb, Dec 19 2017, 06:04:45) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: C:/Users/chris/Documents/FTCC/Web Program Database/P2T1_SalesPrediction_BrownChristopher.py 
+Enter the projected sales: 2000
+The profit is $ 460.00
+>>> 
